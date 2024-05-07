1b167f09a5d49e42119072c505a00a0f09f572c3
19850228 <fmthombothi@yahoo.com>
2024-05-07 17:32:55 +0000

    debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Puppet manifest to fix Apache 500 Internal Server Error
+
+# Ensure Apache and PHP are installed
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
