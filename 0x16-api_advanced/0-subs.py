b43fe6224733c83856c244fdd30cd77e5ed0def7
19850228 <fmthombothi@yahoo.com>
2024-05-07 16:44:56 +0000

    Api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,12 @@
-#!/usr/bin/python3
-'''
-    0-subs
-'''
 import requests
-from sys import argv
-
 
 def number_of_subscribers(subreddit):
-    '''
-        Displays the number of subscribers for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user)
-    try:
-        return url.json().get('data').get('subscribers')
-    except Exception:
-        return None
-
-
-if __name__ == "__main__":
-    subscribers = number_of_subscribers(argv[1])
-    if subscribers is not None:
-        print(subscribers)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
-        print("OK")
+        return 0
